29acdee7a01f4a357a14d24736f07bf8ebc8d4a1
Nikolay Nikolov <nikolay.nikolov@mail.schwarz>
2023-11-11 23:18:19 +0200

    Add more spec Pages based on contry needs

--- fixtures/lidl.ts
+++ fixtures/lidl.ts
@@ -1,7 +1,12 @@
 import {test as base} from "@playwright/test";
 import LidlCookies from "../tests/Lidl/commons/lidlCookies";
 import LidlSearchBar from "../tests/Lidl/psear/searchBar";
+
+//Searh results pages 
+//Common methods for SRP
 import LidlSearchResultsPage from "../tests/lidl/psear/searchResultsPage";
+//1 SK Search results page
+import SKsearchResultsPage from "../tests/Lidl/psear/SKsearchResultsPage";
 
 
 // export const testBase = base.expect<{SearchBar: SearchBar}
@@ -10,6 +15,7 @@ type LidlPages = {
   
   lidlSearchBarPage: LidlSearchBar;
   lidlSearchResultsPage: LidlSearchResultsPage;
+  SKsearchResultsPage: SKsearchResultsPage;
 
 }
 
@@ -26,6 +32,10 @@ export const test = base.extend<LidlPages>({
       },
 
       lidlSearchResultsPage:async ({ page}, use) => {
+        const lidlSearchResultsPage = new LidlSearchResultsPage(page);
+        await use (lidlSearchResultsPage);
+      },
+      SKsearchResultsPage:async ({ page}, use) => {
         const lidlSearchResultsPage = new LidlSearchResultsPage(page);
         await use (lidlSearchResultsPage);
       }
