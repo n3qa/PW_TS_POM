52c485849318a244c238e6ed40130c923f4c0c5b
Nikolay Nikolov <nikolay.nikolov@mail.schwarz>
2023-11-12 01:05:01 +0200

    Refactor Page Object Facotory

---
+++ fixtures/storeFront.ts
@@ -0,0 +1,93 @@
+import { Page ,expect } from "@playwright/test";
+// IMPORT
+// StoreFront common pages
+import LidlCookiesPage from "../tests/Lidl/commons/lidlCookies";
+import LidlSearchBarPage from "../tests/Lidl/psear/searchBar";
+import LidlSearchResultsPage from "../tests/lidl/psear/searchResultsPage";
+
+//IMPORT 
+// Project Spec Pages
+// 1 Cookies 
+// 1.1. SK Cookes 
+ import LidlSKCookiesPage from "../tests/Lidl/commons/SKcookiesPage";
+//1.2. DE Cookies
+import LidlDECookiesPage from "../tests/Lidl/commons/DEcookiesPage";
+//1.3 CZ Cookies
+import LidlCZCookiesPage from "../tests/Lidl/commons/CZcookiesPage";
+//1.4 FR Cookies
+import LidlFRCookiesPage from "../tests/Lidl/commons/FRcookiesPage";
+
+//Searh results pages 
+//Common methods for SRP
+
+//1 SK Search results page
+import LidlSKsearchResultsPage from "../tests/Lidl/psear/SKsearchResultsPage";
+
+// END OF IMPTS 
+export class StoreFront { 
+
+  private readonly page:Page;
+
+  //Providing fields for StoreFront Pages
+  private readonly lidlCookiesPage: LidlCookiesPage;
+  private readonly lidlSearchBarPage: LidlSearchBarPage;
+  private readonly lidlSearchResultsPage: LidlSearchResultsPage;
+  
+  //Providing StoreFront common pages by country
+  //1. Cookies
+  // 1.1. SK 
+  private readonly lidlSKCookiesPage: LidlSKCookiesPage;  
+  private readonly lidlDECookiesPage: LidlDECookiesPage;
+  private readonly lidlCZCookiesPage: LidlCZCookiesPage;
+  private readonly lidlFRCookiesPage: LidlFRCookiesPage;
+  
+  //2. Search results page ->
+  //2.1. SK
+  private readonly lidlSKsearchResultsPage: LidlSKsearchResultsPage;
+
+  constructor(page:Page){
+    this.page = page;
+    
+  //init StoreFront common pages
+   this.lidlCookiesPage = new LidlCookiesPage(this.page);
+   this.lidlSearchBarPage = new LidlSearchBarPage(this.page);
+   this.lidlSearchResultsPage = new LidlSearchResultsPage(this.page);
+    //init Project Spec pages
+    // 1 Cookies 
+    // 1.1. SK Cookes 
+    this.lidlSKCookiesPage = new LidlSKCookiesPage(this.page);
+    // 1.2. DE Cookes 
+    this.lidlDECookiesPage = new LidlDECookiesPage(this.page);
+    // 1.3. CZ Cookes 
+    this.lidlCZCookiesPage = new LidlCZCookiesPage(this.page);
+    // 1.4. FR Cookes 
+    this.lidlFRCookiesPage = new LidlFRCookiesPage(this.page);
+
+    //2.Search Results Page for Countries
+    //2.1. SK SRP 
+    this.lidlSKsearchResultsPage = new LidlSKsearchResultsPage(this.page);
+  }
+
+  //Instance providers
+    //Common pages
+    cookies() {return this.lidlCookiesPage};
+    searchBar(){return this.lidlSearchBarPage};
+    srp() {return this.lidlSearchResultsPage};
+    
+    //Project spec pages
+    // 1 SK Cookies
+    SKCookies(){return this.lidlSKCookiesPage}
+    // 2 DE Cookies
+    DECookiesPage() {return this.lidlDECookiesPage}
+    // 3 FR Cookies
+    FRCookies() {return this.lidlFRCookiesPage}
+    // 4 CZ Cookies
+    CZCookes() {return this.lidlCZCookiesPage}
+    // Search results page -> ABR -> srp
+
+    
+    //Country spec srp -> SK
+    SKsrp() {return this.lidlSKsearchResultsPage}
+
+
+}
