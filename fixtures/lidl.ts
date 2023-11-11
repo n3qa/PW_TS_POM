af7e1507b1caf6deeb76ed11ef053eb1d4bfa848
Nikolay Nikolov <nikolay.nikolov@mail.schwarz>
2023-11-12 01:04:42 +0200

    Create Page Factory for better POM expr

--- fixtures/lidl.ts
+++ fixtures/lidl.ts
@@ -1,61 +1,61 @@
-import {test as base} from "@playwright/test";
-import LidlCookies from "../tests/Lidl/commons/lidlCookies";
-import LidlSearchBar from "../tests/Lidl/psear/searchBar";
+// import {test as base} from "@playwright/test";
+// import LidlCookies from "../tests/Lidl/commons/lidlCookies";
+// import LidlSearchBar from "../tests/Lidl/psear/searchBar";
 
-//Searh results pages 
-//Common methods for SRP
-import LidlSearchResultsPage from "../tests/lidl/psear/searchResultsPage";
-//1 SK Search results page
-import SKsearchResultsPage from "../tests/Lidl/psear/SKsearchResultsPage";
+// //Searh results pages 
+// //Common methods for SRP
+// import LidlSearchResultsPage from "../tests/lidl/psear/searchResultsPage";
+// //1 SK Search results page
+// import SKsearchResultsPage from "../tests/Lidl/psear/SKsearchResultsPage";
 
 
-// export const testBase = base.expect<{SearchBar: SearchBar}
-type LidlPages = {
-  lidlCookiesPage: LidlCookies;
+// // export const testBase = base.expect<{SearchBar: SearchBar}
+// type LidlPages = {
+//   lidlCookiesPage: LidlCookies;
   
-  lidlSearchBarPage: LidlSearchBar;
-  lidlSearchResultsPage: LidlSearchResultsPage;
-  SKsearchResultsPage: SKsearchResultsPage;
+//   lidlSearchBarPage: LidlSearchBar;
+//   lidlSearchResultsPage: LidlSearchResultsPage;
+//   SKsearchResultsPage: SKsearchResultsPage;
 
-}
+// }
 
-export const test = base.extend<LidlPages>({
+// export const test = base.extend<LidlPages>({
 
-      lidlCookiesPage: async ({page}, use) => {
-        const lidlCookies = new LidlCookies(page); 
-        await use (lidlCookies);
-      }, 
+//       lidlCookiesPage: async ({page}, use) => {
+//         const lidlCookies = new LidlCookies(page); 
+//         await use (lidlCookies);
+//       }, 
 
-      lidlSearchBarPage:async ({page},use) => {
-        const lidlSearchBar = new LidlSearchBar(page);
-        await use (lidlSearchBar);
-      },
+//       lidlSearchBarPage:async ({page},use) => {
+//         const lidlSearchBar = new LidlSearchBar(page);
+//         await use (lidlSearchBar);
+//       },
 
-      lidlSearchResultsPage:async ({ page}, use) => {
-        const lidlSearchResultsPage = new LidlSearchResultsPage(page);
-        await use (lidlSearchResultsPage);
-      },
-      SKsearchResultsPage:async ({ page}, use) => {
-        const lidlSearchResultsPage = new LidlSearchResultsPage(page);
-        await use (lidlSearchResultsPage);
-      }
-}); 
+//       lidlSearchResultsPage:async ({ page}, use) => {
+//         const lidlSearchResultsPage = new LidlSearchResultsPage(page);
+//         await use (lidlSearchResultsPage);
+//       },
+//       SKsearchResultsPage:async ({ page}, use) => {
+//         const lidlSearchResultsPage = new LidlSearchResultsPage(page);
+//         await use (lidlSearchResultsPage);
+//       }
+// }); 
 
 
-// export const test = base.expect<{
-//   searchBar: SearchBar; 
-//   searchResultsPage: SearchResultsPage;
-//   cookies: Cookies;
+// // export const test = base.expect<{
+// //   searchBar: SearchBar; 
+// //   searchResultsPage: SearchResultsPage;
+// //   cookies: Cookies;
 
-// }> ({
-//   searchBar: async ({page}, use) => {
-//      await use (new SearchBar(page));
-//   },
-//   searchResultsPage: async ({page}, use) => {
-//     await use (new SearchResultsPage(page));
-//   },
-//   cookies: async ({page}, use) => {
-//     await use (new Cookies(page));
-//   }
+// // }> ({
+// //   searchBar: async ({page}, use) => {
+// //      await use (new SearchBar(page));
+// //   },
+// //   searchResultsPage: async ({page}, use) => {
+// //     await use (new SearchResultsPage(page));
+// //   },
+// //   cookies: async ({page}, use) => {
+// //     await use (new Cookies(page));
+// //   }
 
-// });
+// // });
---
+++ tests/Lidl/commons/SKcookiesPage.ts
@@ -0,0 +1,44 @@
+  import { Expect, Locator, Page, expect } from "@playwright/test";
+
+  export default class SKCookiesPage {
+    readonly page: Page;
+    constructor(page: Page) {
+      this.page = page;
+    }
+
+  //SK flow 
+  //Locators
+  SK_acceptAll_cs = () => this. page.getByRole('button', { name: 'POVOLIŤ' });
+  SK_srejectAll_cs = () => this.page.locator('label').filter({ hasText: 'Štatistické' }).getByRole('switch', { name: 'Štatistické' });
+
+  SK_preferences_cs = () => this.page.locator('label').filter({ hasText: 'Preferenčné' }).getByRole('switch', { name: 'Preferenčné' });
+  SK_statistics_cs = () => this.page.locator('label').filter({ hasText: 'Štatistické' }).getByRole('switch', { name: 'Štatistické' });
+  SK_marketing_cs = () => this.page.locator('label').filter({ hasText: 'Marketingové' }).getByRole('switch', { name: 'Marketingové' });
+  SK_submitMoreCS = () => this.page.locator('label').filter({ hasText: 'Marketingové' }).getByRole('switch', { name: 'Marketingové' });;
+
+  //Actions
+ 
+  public async SK_clickOn_AcceptAllCS_btn() {
+    await this.SK_acceptAll_cs().click();
+  }
+
+  public async SK_clickOn_RejectAllCS_btn() {
+    await this.SK_srejectAll_cs().click();
+  }
+
+  
+  public async SK_clickOn_Preferences_btn() {
+    await this.SK_preferences_cs().click();
+  }
+
+  public async SK_clickOn_Statistics_btn() {
+    await this.SK_statistics_cs().click();
+  }
+
+  public async SK_clickOn_Marketing_btn() {
+    await this.SK_preferences_cs().click();
+  }
+
+    
+
+  }
