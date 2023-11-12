bc8912553e9d9311fe18b67dd4ad7d63d6324fb7
Nikolay Nikolov <nikolay.nikolov@mail.schwarz>
2023-11-12 19:56:41 +0200

    Fix GLobalConfiger typo

--- lidl-configurator.ts
+++ lidl-configurator.ts
@@ -1,29 +1,11 @@
-import { PlaywrightTestOptions, test as base } from '@playwright/test'
+import { test as base } from '@playwright/test';
 
+export type TestOptions = {
+  globalQAurl: string;
+  globalPRODurl:string;
+};
 
-/*
-import { defineConfig } from '@playwright/test';
-export default defineConfig({
-  use: {
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: 'on-first-retry',
-  },
+export const test = base.extend<TestOptions>({  
+  globalQAurl: ['', { option: true }],
+  globalPRODurl: ['', { option: true }],
 });
-
-*/
-export type LidlConfigurator = {
-    globalQAurlSK: string
-    globalPRODurlSK: string
-} 
-
-export const test = base.extend<PlaywrightTestOptions> ({
-    // Set up global var for country: 
-   
-    // SK    => 'https://qa.lidl.sk/', 'https://www.lidl.sk/'
-    globalQAur: ['',{option: true}],
-    globalPRODur: ['',{option: true}],
- 
-
-})
