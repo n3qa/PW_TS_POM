d0f20f01022be9e8d5f4d0c42f2f0c3a485b5545
Nikolay Nikolov <nikolay.nikolov@mail.schwarz>
2023-11-21 21:52:45 +0200

    Starting to build common methods for price facet

--- tests/SK/qaSKfacetPrice.spec.ts
+++ tests/SK/qaSKfacetPrice.spec.ts
@@ -1,183 +1,183 @@
 import { test } from "../../fixtures/lidl-configurator";
 
 
-test("SK - 1 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+    test("SK - 1 -QA - SEARCH-VIEW | SERP | Facet | Price |" +
+        "Verify Price facet can be collapsed and expanded" ,
+        async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
+        //STEP 2: 
+        //CLick on Accept all cookies oneTrust cookie settings button
         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
-
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase1();
+        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn_qa();
+ 
+        //STEP 3:
+        // The user submits a basic search on the home page
+        console.log("*** STEP 3: The user submits a basic search on the home page");
+        await lidlStoreFront.searchBar().submitSearchQuery("*");
+            
+        await lidlStoreFront.facetPrice().validate_facet_price_initial_props_for_sk();
+     
     });
 
 
-test("SK - 2 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 2 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase2();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase2();
+//     });
 
-test("SK - 3 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 3 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase3();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase3();
+//     });
 
-test("SK - 4 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 4 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase4();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase4();
+//     });
 
-test("SK - 5 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 5 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase5();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase5();
+//     });
 
-test("SK - 6 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 6 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase6();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase6();
+//     });
 
-test("SK - 7 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 7 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase7();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase7();
+//     });
 
-test("SK - 8 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 8 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase8();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase8();
+//     });
 
-test("SK - 9 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 9 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase9();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase9();
+//     });
 
-test("SK - 10 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
-    " SINGLE increment | TOP > DOWN "+
-    "Verify user can successfully navigate " +
-    "from 1st category level " +
-    "to 2nd category level",
-    async ({lidlStoreFront }) => {
+// test("SK - 10 -QA - SEARCH-VIEW | SERP | Facet | Category |" +
+//     " SINGLE increment | TOP > DOWN "+
+//     "Verify user can successfully navigate " +
+//     "from 1st category level " +
+//     "to 2nd category level",
+//     async ({lidlStoreFront }) => {
 
-//STEP 2: 
-//CLick on Accept all cookies oneTrust cookie settings button
-        console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
-        await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
+// //STEP 2: 
+// //CLick on Accept all cookies oneTrust cookie settings button
+//         console.log("STEP 2: CLick on oneTrust accept all cookies settings button");
+//         await lidlStoreFront.SKCookies().clickOn_AcceptAllCS_btn();
 
-//STEP  0:
-// The user submits a basic search on the home page
-        console.log("STEP 3: The user submits a basic search on the home page");
-        await lidlStoreFront.SKfacetPrice().debugTestCase10();
-    });
+// //STEP  0:
+// // The user submits a basic search on the home page
+//         console.log("STEP 3: The user submits a basic search on the home page");
+//         await lidlStoreFront.SKfacetPrice().debugTestCase10();
+//     });
